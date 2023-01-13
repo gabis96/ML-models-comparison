711cb1b658117e612b2b15e79aa71cfca76de967
Gabriela Rodriguez <103657707+gabis96@users.noreply.github.com>
2023-01-13 17:01:02 +0000

    Add maps to report.

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -23,13 +23,16 @@ class WorldHappinessVisualization:
         plt.figure(figsize=figsize)
         heatmap = sns.heatmap(corr, vmin=-1, vmax=1, cmap=sns.color_palette(palette=palette, as_cmap=True), annot=True, fmt=".1f")
         heatmap.set_title('Correlations between Features', fontdict={'fontsize':10}, pad=22)
+        return heatmap
 
     def bivariate_happiness_plot(self, figsize=(26, 5), palette=None):
         if palette is None:
             palette=self.palette
-        
+            
+        relevant_features = ['Freedom', 'Generosity', 'Healthy life expectancy', 'Perceptions of corruption', 'GDP per capita', 'Social support']
         f = Figure(figsize, layout="constrained")
-        (
+        
+        return (
             so.Plot(self.df, y='Happiness Score', color='Happiness Score')
             .add(so.Dots())
             .pair(x=relevant_features)
@@ -37,5 +40,19 @@ class WorldHappinessVisualization:
             .on(f)
         )
 
-    def pair_plot(self, figsize=(8,6), palette=None):
-        pass
+    def pair_plot(self, palette=None):
+        if palette is None:
+            palette=self.palette
+
+        return sns.pairplot(self.df, palette=palette)
+
+    def pie_plot(self, figsize=(8,6), palette=None):
+        if palette is None:
+            palette=self.palette
+
+        nhappy = self.df[self.df['Happiness Score'] > 5].shape[0]
+        nunhappy = self.df[self.df['Happiness Score'] < 5].shape[0]
+
+        return plt.pie([nhappy, nunhappy], labels=['Happy', 'Unhappy'], colors=['#87b7e0', '#cf604a'], autopct='%.0f%%')  
+
+
